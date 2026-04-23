a4e4dfc4c176181068ed5eefcce9f81c47d8de22
Jared Adem <Demabior123@gmail.com>
2026-04-23 14:03:42 -0400

    Setup Tailwind CSS successfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>Kademic Platform 🚀</h1>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-green-600">
+        Kademic Platform 🚀
+      </h1>
     </div>
   )
 }
